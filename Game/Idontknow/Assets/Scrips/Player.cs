f80ce74417a48ae0a66c09fee9aeed7e849187a9
MSI\Jan Velema <velema98@gmail.com>
2017-12-11 17:08:25 +0100

    Player movement upgraded -Wall jumps added -Slipery physics added
    Testcourse added

--- Game/Idontknow/Assets/Scrips/Player.cs
+++ Game/Idontknow/Assets/Scrips/Player.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    
-    public float speed;
-    public float jumpSpeed;
+
+    //Rigidbody
     private Rigidbody rb;
 
-    float distToGround;
-    Collider col;
+    //X movement
+    public float speed;
+    public float minSpeed;
+    public float maxSpeed;
+
+    //drag
+    public float drag;
+    public bool airDragTest;
+
+    //Y movement
+    public float jumpSpeed;
+    public float airJumpMultiplier;
+    public int maxAirJumps;
+    int airJumps;
+    
+    //gravity
+    public float gravity;
+    public float wallGravity;
+    
+    //collision ditection
+    private Collider col;
+    private float distToGround;
+    private float distToWall;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +38,7 @@ public class Player : MonoBehaviour {
         col = gameObject.GetComponent<Collider>();
         // get the distance to ground
         distToGround = col.bounds.extents.y;
+        distToWall = col.bounds.extents.x;
     }
 
     //Checks if the player in on the ground using 5 raycast to minimize the area not checked and returns a boolian
@@ -31,31 +52,130 @@ public class Player : MonoBehaviour {
         return OnGround;
     }
 
-	// Update is called once per frame
-	void Update ()
+    //Checks if the player is on the left wall
+    bool OnLeftWall()
+    {
+        bool OnWall1 = Physics.Raycast(transform.position, Vector3.left, distToWall + 0.1f);
+        bool OnWall = OnWall1;
+        return OnWall;
+    }
+
+    //Checks if the player is on the right wall
+    bool OnRightWall()
+    {
+        bool OnWall1 = Physics.Raycast(transform.position, Vector3.right, distToWall + 0.1f);
+        bool OnWall = OnWall1;
+        return OnWall;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         //Controls
         {
+            //Simulate gravity for player only 
+            if (!IsGrounded())
+            {
+                //Gravity on a wall is lower while moving down
+                if ((OnLeftWall() || OnRightWall()) && (rb.velocity.y < 0))
+                {
+                    rb.AddForce(0, -wallGravity, 0);
+                }
+                else
+                {
+                    rb.AddForce(0, -gravity, 0);
+                }
+            }
+
+            //Limmit the speed a player can move at
+            if (rb.velocity.x > maxSpeed)
+            {
+                rb.velocity = new Vector3(maxSpeed, rb.velocity.y, 0);
+            }
+            if (rb.velocity.x < -maxSpeed)
+            {
+                rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, 0);
+            }
+
+            //Reset doublejumps when on the ground
+            if (IsGrounded())
+            {
+                airJumps = maxAirJumps;
+            }
+
+            //Movement controls
             if (Input.GetKey(KeyCode.LeftArrow) && !(Input.GetKey(KeyCode.RightArrow)))
             {
-                rb.AddForce(new Vector3(-speed, rb.velocity.y, 0) - rb.velocity, ForceMode.VelocityChange);
+                rb.AddForce(-speed, 0, 0);
             }
             if (Input.GetKey(KeyCode.RightArrow) && !(Input.GetKey(KeyCode.LeftArrow)))
             {
-                rb.AddForce(new Vector3(speed, rb.velocity.y, 0) - rb.velocity, ForceMode.VelocityChange);
-            }
-            if (!((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.LeftArrow))) || ((Input.GetKey(KeyCode.RightArrow)) && (Input.GetKey(KeyCode.LeftArrow))))
-            {
-                rb.AddForce(new Vector3(0, rb.velocity.y, 0) - rb.velocity, ForceMode.VelocityChange);
+                rb.AddForce(speed, 0, 0);
             }
 
-            if ((Input.GetKeyDown(KeyCode.UpArrow)) && IsGrounded())
+            //If no or both directions are pressed slow down using drag
+            if (!((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.LeftArrow))) || ((Input.GetKey(KeyCode.RightArrow)) && (Input.GetKey(KeyCode.LeftArrow))))
             {
-                rb.AddForce(new Vector3(rb.velocity.x, jumpSpeed, 0) - rb.velocity, ForceMode.VelocityChange);
+                //Minimal speed to avoid micromovements instead of stopping
+                if (rb.velocity.x < minSpeed && rb.velocity.x > -minSpeed)
+                {
+                    rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                }
+
+                //Apply drag when on ground or when airDrag is turned on
+                if (IsGrounded() || airDragTest)
+                {
+                    if (rb.velocity.x >= minSpeed)
+                    {
+                        rb.AddForce(-drag, 0, 0);
+                    }
+                    if (rb.velocity.x <= -minSpeed)
+                    {
+                        rb.AddForce(drag, 0, 0);
+                    }
+                }
             }
+
+            //Press up to jump
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                //Check if the player not on a wall in the air or if the player in on the ground
+                if (!(OnLeftWall() || OnRightWall()) || IsGrounded()){
+                    //Jumping while on the ground is higher
+                    if (IsGrounded())
+                    {
+                        rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.VelocityChange);
+                    }
+                    //Check if any air jumps are remaining and airjumps are effected by airjump multiplier
+                    else if (airJumps > 0)
+                    {
+                        rb.AddForce(new Vector3(0, jumpSpeed * airJumpMultiplier, 0) - new Vector3(0, rb.velocity.y, 0), ForceMode.VelocityChange);
+                        airJumps--;
+                    }
+                }
+                //If on a wall in the air do a walljump
+                else
+                {
+                    //On Left wall jump right
+                    if (OnLeftWall())
+                    {
+                        rb.AddForce(new Vector3(jumpSpeed, jumpSpeed, 0) - new Vector3(0, rb.velocity.y, 0), ForceMode.VelocityChange);
+                    }
+                    //On right wall jumo left
+                    if (OnRightWall())
+                    {
+                        rb.AddForce(new Vector3(-jumpSpeed, jumpSpeed, 0) - new Vector3(0, rb.velocity.y, 0), ForceMode.VelocityChange);
+                    }
+                }
+            }
+            //While in the air press down to cancel jump and fast fall down
             if ((Input.GetKeyDown(KeyCode.DownArrow)) && !IsGrounded())
             {
-                rb.AddForce(new Vector3(rb.velocity.x, -jumpSpeed, 0) - rb.velocity, ForceMode.VelocityChange);
+                //Limmit the downwards speed achieved with downfall
+                if (rb.velocity.y > (-3 * jumpSpeed))
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, -3 * jumpSpeed, 0);
+                }
             }
         }
     }
